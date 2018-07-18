feed948890e8e3b6a9d8540d62eb72271340dd7b
_TheJMC_ <legojames99@gmail.com>
2018-07-18 16:30:51 +0100

    Edited Install.py

--- .vscode/install.py
+++ .vscode/install.py
@@ -6,7 +6,7 @@ def install():
     print("Installing the required packages...")
     try:
         print("Installing...")
-        # pipmain(['install', 'appJar'])
+        # pipmain(['install', 'appJar'])    
         os.system('git clone https://github.com/QPJAY/InstLife-Game.git')
     except:
         print("Something has not worked in the installation phase. Either the required package is already installed, or pip broke!")
